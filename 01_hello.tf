73a25976f15c6af73f96369ea15311bef03a18f5
Pratham Sharma <sharmapratham220@gmail.com>
2024-01-26 11:35:04 +0530

    second file

--- 01_hello.tf
+++ 01_hello.tf
@@ -5,11 +5,20 @@
 # //}
 
 
-# output "hello_file" {
-#     value="Hello !, GitHub"
-# }
+output "hello1" {
+    value="Hello Github !!"
+  
+}
 
+output "hello2" {
+    value="Hello My name is Khan and I am not a terrorist !!"
+  
+}
 
-# // save file
-# // go to cmd 
-# // type terraform plan and see the magic
+output "hello2"{
+    value="Hello Docker"
+}
+
+output "hello3"{
+    value="This is me, from K8's"
+}
--- 02_varaible.tf
+++ 02_varaible.tf
@@ -1,22 +1,42 @@
-# variable "username" {}
+# Terraform script defining variables for usernames, roll number, greetings, and age with corresponding outputs.
 
-# output "helloG" {
-#   value="Hello, ${var.username}"
-# }
 
-// cmd
-// terraform plan and type the value
-
-// USING DEFAULT VALUE
-
-variable "username"{
-    default = GitHub
+# Define a variable for a generic username with a default value "World".
+variable "username" {
+    default = "World"
 }
 
-output "helloG" {
-    value="Hello , ${var.username}"
-  
+# Define a variable for roll number without a default value; it needs to be provided during Terraform execution.
+variable "rollnumber" {}
+
+# Output a greeting string using the username variable.
+output "PrintName" {
+    value = "Hello ! ${var.username}"
 }
 
-// cmd
-// terraform plan 
+# Output a statement incorporating the roll number variable.
+output "roll" {
+    value = "Your Roll.No is : ${var.rollnumber}"
+}
+
+# Define a variable for a specific username with a default value "Github" and a specified type (string).
+variable "username1" {
+    type    = string
+    default = "Github"
+}
+
+# Define a variable for age with a default value 21 and a specified type (number).
+variable "age1" {
+    type    = number
+    default = 21
+}
+
+# Output a greeting using the specific username variable.
+output "Hello1" {
+    value = "Hello, ${var.username1}"
+}
+
+# Output a statement about age using the age variable.
+output "Hello2" {
+    value = "My age is ${var.age1}"
+}
