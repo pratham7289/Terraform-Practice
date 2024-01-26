f5854da9d65f581d937b41360155239164328dbf
Pratham Sharma <sharmapratham220@gmail.com>
2024-01-26 11:25:52 +0530

    First file

---
+++ 01_hello.tf
@@ -0,0 +1,15 @@
+# sample format
+
+// block "lable1" lable2 {
+    //identifier=expression
+//}
+
+
+output "hello_file" {
+    value="Hello !, GitHub"
+}
+
+
+// save file
+// go to cmd 
+// type terraform plan and see the magic
